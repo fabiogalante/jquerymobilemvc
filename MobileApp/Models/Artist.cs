50547bab671425b3dc2a30dda7f564ac12c4a982
devmediatestegits <devmedia2014@outlook.com>
2015-03-10 19:23:33 -0300

    mobile

---
+++ MobileApp/Models/Artist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MobileApp.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+    public class MvcMusicStoreContext : DbContext
+    {
+        public MvcMusicStoreContext()
+            : base("Name=MvcMusicStoreContext")
+        {
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
